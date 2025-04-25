e02a66a8ca46f290daa8be1899822eeab6a07944
AaronGutierrez-Dev <aarongutierrez229@gmail.com>
2025-04-24 22:26:58 -0500

    Actualizacion de estado

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,19 @@
-import java.sql.SQLOutput;
+import com.aluracursos.screenmatch.modelos.Pelicula;
 
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.fechaDeLanzamiento = 2021;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2016);
+        miPelicula.setDuracionEnMinutos(280);
+        miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(7.8);
         miPelicula.evalua(10);
         miPelicula.evalua(9.1);
-        System.out.println(miPelicula.sumaDeEvaluaciones);
-
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 1998;
-        otraPelicula.duracionEnMinutos = 180;
-        otraPelicula.muestraFichaTecnica();
+        System.out.println(miPelicula.getTotalEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
 
 
     }
